38fc0c784ac6c1f6c1ee24c8d09be098ea983b5d
AbhinavS8 <abhinav.subr@gmail.com>
2025-03-01 12:12:13 +0530

    added form validation

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes,Link} from 'react-router-dom'
 import Home from "./pages/Home"
+import CreateAct from "./pages/CreateAct"
+
 
 function App() { //route structure
 
     return (
     <div>
     <Router>
+      <Link to="/createactivity">goes to creating activity route!</Link>
+      <Link to="/">goes to home!</Link>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/createactivity" element={<CreateAct />} />
       </Routes>
     </Router>
     </div>
---
+++ client/src/pages/CreateAct.js
@@ -0,0 +1,62 @@
+import {Formik, Form, Field, ErrorMessage} from "formik"
+import * as Yup from "yup"
+import axios from "axios"
+
+function CreateAct() {
+    const initialValues = {
+        name:"sigma",
+        // start_time:"",
+        // end_time:""
+    }
+
+    const onSubmit = (data) => {
+        axios.post("http://localhost:3001/activities", data).then((response) => {
+            console.log(data);
+        })
+    }
+
+    const validationSchema = Yup.object({
+        name: Yup.string().required().min(1),
+        // start_time: Yup.required(),
+        // end_time: Yup.required()
+    })
+
+
+    return(
+        <div className="createActivityPage">
+            <Formik 
+                initialValues={initialValues}
+                onSubmit={onSubmit}
+                validationSchema={validationSchema}
+            >
+                
+                <Form>
+                    <label>name: </label>
+                    <ErrorMessage name="name" component="span"/>
+                    <Field 
+                    id="inputCreateActivity"
+                    name="name"
+                    placeholder="enter activity name"
+                    />
+
+                    {/* <label>start time: </label>
+                    <ErrorMessage name="start_time" component="span"/>
+                    <Field 
+                    id="inputCreateActivity"
+                    name="start_time"
+                    />
+
+                    <label>end time: </label>
+                    <ErrorMessage name="end_time" component="span"/>
+                    <Field 
+                    id="inputCreateActivity"
+                    name="end_time"
+                    /> */}
+                    <button type="submit">Create Activity</button>
+                </Form>
+            </Formik>
+        </div>
+    )
+}
+
+export default CreateAct
