d3f75579ba812c3a71240b5233301226e93d8450
AbhinavS8 <abhinav.subr@gmail.com>
2025-03-08 12:47:06 +0530

    added schedule endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import CreateAct from "./pages/CreateAct";
 import Activity from "./pages/Activity";
+import Schedule from "./pages/Schedule";
 
 function App() {
   return (
@@ -11,11 +12,13 @@ function App() {
         <nav className="navbar">
           <Link to="/">Home</Link>
           <Link to="/createactivity">Create Activity</Link>
+          <Link to="/Schedule">View Schedule</Link>
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/createactivity" element={<CreateAct />} />
           <Route path="/activity/:id" element={<Activity />} />
+          <Route path="/activity/schedule" element={<Schedule />} />
         </Routes>
       </Router>
     </div>
--- server/routes/Activities.js
+++ server/routes/Activities.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { Activities } = require("../models");
+const { Op } = require("sequelize");
 
 router.get("/", async (req, res) => {
     const listActivities = await Activities.findAll();
@@ -11,6 +12,20 @@ router.get("/byId/:id", async (req, res) => {
     const id = req.params.id
     const post = await Activities.findByPk(id)
     res.json(post)
+})
+
+router.get("/schedule", async(req,res) => {
+    const activities = await Activities.findAll({
+        where: {
+            status:"to do",
+            start_time: {
+                [Op.like]: "0_:__"
+            }
+        }
+    }
+);
+res.json(activities);
+
 })
 
 router.post("/",async (req,res)=>{
